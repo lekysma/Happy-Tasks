01188e7665b768f73b19e0e0b92e8b2dc20b1498
lekysma <jeanmartinkyssama@yahoo.com>
2019-09-14 10:52:33 +0000

    creation des deux VC de mon application. mise en place de C de 'CRUD'. cependant il y a un bug : des taches ajoutées a une catégorie disparaissent quand on revient en arrière. A faire ; trouver code pour afficher le 'Filepath' pour les données CoreData, écrire code pour R de 'CRUD' afin de charger les données et voir s'il ya un problème avec la fonction sauvegarder

---
+++ Happy Tasks/Controllers/CategoriesTableViewController.swift
@@ -0,0 +1,114 @@
+//
+//  CategoriesTableViewController.swift
+//  Happy Tasks
+//
+//  Created by Jean martin Kyssama on 10/09/2019.
+//  Copyright © 2019 Jean martin Kyssama. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+class CategoriesTableViewController: UITableViewController {
+    // on cree le context
+    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+
+    // on cree ici le tableau qui contient les differentes categories
+    var tableauCategorie = [Categorie]()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        listeCategories()
+
+        
+    }
+
+    // MARK: - Table view data source
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return tableauCategorie.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "celluleCategorie", for: indexPath)
+        
+        let indexPathVariable = tableauCategorie[indexPath.row]
+        
+        cell.textLabel?.text = indexPathVariable.nom
+        // se charge de la taille du texte
+        cell.textLabel?.font = UIFont(name: (cell.textLabel?.font.fontName)!, size: 25)
+        
+        cell.imageView?.image = UIImage(named: indexPathVariable.image!)
+        
+        return cell
+    }
+    
+
+    // MARK: - table view delegate
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        performSegue(withIdentifier: "versTaches", sender: self)
+    }
+    
+    
+    // MARK: - data manipulation methods
+    
+    func sauvegardeCategories() {
+        
+        do {
+            try context.save()
+        } catch {
+            print("Erreur dans la sauvegarde du contexte\(error)")
+        }
+    }
+    
+    //
+    
+    func chargementCategories() {
+        
+        
+        
+        do {
+            
+        } catch {
+            print("Erreur dans le chargement des données sauvegardées\(error)")
+        }
+    }
+    
+    // MARK: - Liste des categories
+    func listeCategories() {
+        // on cree les differents objets et on les ajoute au tableau de categories
+        let premiereCategorie = Categorie(context: context)
+        premiereCategorie.nom = "Travail"
+        premiereCategorie.image = "1"
+        tableauCategorie.append(premiereCategorie)
+        //
+        let secondeCategorie = Categorie(context: context)
+        secondeCategorie.nom = "Courses"
+        secondeCategorie.image = "2"
+        tableauCategorie.append(secondeCategorie)
+        //
+        let troisiemeCategorie = Categorie(context: context)
+        troisiemeCategorie.nom = "Famille"
+        troisiemeCategorie.image = "3"
+        tableauCategorie.append(troisiemeCategorie)
+        //
+        let quatriemeCategorie = Categorie(context: context)
+        quatriemeCategorie.nom = "Miscelanous"
+        quatriemeCategorie.image = "4"
+        tableauCategorie.append(quatriemeCategorie)
+        
+        // une fois cela fait, on enregistre le contexte
+        do {
+            try context.save()
+            
+        } catch {
+            print("Impossible de sauvegarder les données dans le contexte\(error)")
+        }
+        
+        tableView.reloadData()
+        
+    }
+    
+    
+    
+}
---
+++ Happy Tasks/Controllers/TachesViewController.swift
@@ -0,0 +1,111 @@
+//
+//  ViewController.swift
+//  Happy Tasks
+//
+//  Created by Jean martin Kyssama on 10/09/2019.
+//  Copyright © 2019 Jean martin Kyssama. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+class TachesViewController: UITableViewController {
+    
+    // on cree un tableau qui enregistre tous les elements
+    var tableauTaches = [Tache]()
+    // on cree le contexte
+    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    // MARK: - Table view delegate
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        // Chaque fois qu'on clique sur une cellule on s'assure que sa proprieté 'done' change
+        tableauTaches[indexPath.row].done.toggle()
+        
+        // on ajoute une animation
+        tableView.deselectRow(at: indexPath, animated: true)
+        
+        // et on sauvegarde les donnees
+        sauvegardeTaches()
+        
+    }
+   
+    // MARK: - Table view data source
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return tableauTaches.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "celluleTache", for: indexPath)
+        let tacheIndexPathRow = tableauTaches[indexPath.row]
+        
+        cell.textLabel?.text = tacheIndexPathRow.nom
+        cell.textLabel?.font = UIFont(name: (cell.textLabel?.font.fontName)!, size: 18)
+        
+        // ici on gere le fait d'affecter un checkmark a une cellule dont la propriete done est positive et vice versa
+        if tacheIndexPathRow.done == true {
+            cell.accessoryType = .checkmark
+        } else {
+            cell.accessoryType = .none
+        }
+        
+        return cell
+    }
+    
+    
+    // MARK: - Data manipulation methods
+    
+    //Sauvegarde
+    func sauvegardeTaches() {
+        do {
+            try context.save()
+            
+        } catch {
+            print("Impossible de sauvegarder les données\(error)")
+        }
+        // et on recharge la table view
+        tableView.reloadData()
+        
+    }
+    //Chargement
+    func chargementTaches() {
+        
+    }
+    
+    // MARK: - Ajout d'une nouvelle tache
+    
+    @IBAction func ajoutNouvelleTache(_ sender: UIBarButtonItem) {
+        
+        var textField = UITextField()
+        let alert = UIAlertController(title: "Nouvelle tâche", message: "", preferredStyle: .alert)
+        let action = UIAlertAction(title: "Ajouter la tâche", style: .default) { (UIAlertAction) in
+            // quand on clique sur le bouton, toute l'action se passe ici
+            
+            let nouvelleTache = Tache(context: self.context)
+            nouvelleTache.nom = textField.text
+            nouvelleTache.done = false
+            self.tableauTaches.append(nouvelleTache)
+            
+            // on sauvegarde
+            self.sauvegardeTaches()
+            
+        }
+        // action et textField
+        alert.addAction(action)
+        alert.addTextField { (UITextField) in
+            UITextField.placeholder = "Tapez ici"
+            textField = UITextField
+        }
+        
+        // presentation
+        present(alert, animated: true, completion: nil)
+    }
+    
+
+
+}
+
